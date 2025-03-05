41aa1e4dcf0830e9cd6bc548a125b6c919a7ac9c
Ankit Jha <professionalaj209@gmail.com>
2025-03-05 11:55:44 +0530

    user controller defined

---
+++ prisma/migrations/20250305054339_initalizing/migration.sql
@@ -0,0 +1,52 @@
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('USER', 'ADMIN');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "username" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "phoneNo" INTEGER,
+    "dob" TIMESTAMP(3),
+    "role" "Role" NOT NULL DEFAULT 'USER',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Blog" (
+    "id" SERIAL NOT NULL,
+    "userId" INTEGER NOT NULL,
+
+    CONSTRAINT "Blog_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Like" (
+    "id" SERIAL NOT NULL,
+    "blogId" INTEGER NOT NULL,
+    "userId" INTEGER NOT NULL,
+
+    CONSTRAINT "Like_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_phoneNo_key" ON "User"("phoneNo");
+
+-- AddForeignKey
+ALTER TABLE "Blog" ADD CONSTRAINT "Blog_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Like" ADD CONSTRAINT "Like_blogId_fkey" FOREIGN KEY ("blogId") REFERENCES "Blog"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Like" ADD CONSTRAINT "Like_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
---
+++ src/controllers/user.controller.ts
@@ -0,0 +1,129 @@
+import { PrismaClient } from "@prisma/client";
+import bcrypt from 'bcrypt';
+import { Request, Response } from "express";
+import jwt from 'jsonwebtoken'
+import dotenv from 'dotenv'
+
+dotenv.config();
+const client = new PrismaClient();
+
+export const signup = async (req: Request, res: Response) => {
+    try {
+        const {username, email, password} = req.body;
+        if(!username || !email || !password){
+            res.status(404).json({
+                success: false,
+                message: "Need all the Information"
+            })
+            return;
+        }
+        if(username.length < 5 || password.length < 6){
+            res.status(401).json({
+                success: false,
+                message: "Username should have minimum 5 words & Password should contain minimum 6 words"
+            })
+            return;
+        }
+        const user = await client.user.findFirst({
+            where:{
+                username
+            }
+        })
+        if(user){
+            res.status(402).json({
+                success: false,
+                message: "Username Already Exist"
+            })
+            return;
+        }
+        const emailExist = await client.user.findFirst({
+            where:{
+                email
+            }
+        })
+        if(emailExist){
+            res.status(402).json({
+                success: false,
+                message: "Email Already Exist"
+            })
+            return;
+        }
+
+        const hashPass = await bcrypt.hash(password, 11)
+        await client.user.create({
+            data:{
+                username,
+                password: hashPass,
+                email
+            }
+        })
+        res.status(200).json({
+            success: true,
+            message: "User Created Successfully"
+        })
+        return;
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({
+            success: false,
+            message: "Error while Creating User"
+        })
+        return
+    }
+}
+
+
+export const signin = async (req: Request, res: Response) => {
+    try {
+        const {email, password} = req.body;
+        if(!email || !password){
+            res.status(404).json({
+                success: false,
+                message: "All the fields are required"
+            })
+            return ;
+        }
+        const userExist = await client.user.findFirst({
+            where: {
+                email
+            }
+        })
+        if(!userExist){
+            res.status(401).json({
+                success: false,
+                message: "User Does not exist"
+            })
+            return;
+        }
+        if(await bcrypt.compare(password, userExist.password)){
+            const payload = {
+                id: userExist.id
+            }
+            const token = jwt.sign(payload, process.env.JWT_SECRET || "secret_key", {
+                expiresIn: '1h'
+            })
+            res.status(200).json({
+                success: true,
+                message: "Successfully Logged In",
+                token
+            })
+            return;
+        }
+        else{
+            res.status(403).json({
+                success: false,
+                message: "Password is not correct"
+            })
+            return;
+        }
+
+        
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({
+            success: false,
+            message: "Error while Signing Up"
+        })
+        return;
+    }
+}
